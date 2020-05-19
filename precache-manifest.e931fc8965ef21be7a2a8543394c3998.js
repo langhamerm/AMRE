2046e22a7d3182ec476a6817e63128f96f3611f0
langhamerm <43483751+langhamerm@users.noreply.github.com>
2020-05-18 22:38:57 -0500

    Update precache-manifest.e931fc8965ef21be7a2a8543394c3998.js

--- precache-manifest.e931fc8965ef21be7a2a8543394c3998.js
+++ precache-manifest.e931fc8965ef21be7a2a8543394c3998.js
@@ -1,30 +1,30 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "e9f1367a2194a874e771b7ca2ad578ad",
-    "url": "/amre/index.html"
+    "url": "/index.html"
   },
   {
     "revision": "451aafdd4a21c9ec8303",
-    "url": "/amre/static/css/2.af3c1da9.chunk.css"
+    "url": "/static/css/2.af3c1da9.chunk.css"
   },
   {
     "revision": "13ddbbabdceee06f73e4",
-    "url": "/amre/static/css/main.7846461a.chunk.css"
+    "url": "/static/css/main.7846461a.chunk.css"
   },
   {
     "revision": "451aafdd4a21c9ec8303",
-    "url": "/amre/static/js/2.a6dffdb0.chunk.js"
+    "url": "/static/js/2.a6dffdb0.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/amre/static/js/2.a6dffdb0.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.a6dffdb0.chunk.js.LICENSE.txt"
   },
   {
     "revision": "13ddbbabdceee06f73e4",
-    "url": "/amre/static/js/main.c1d923d6.chunk.js"
+    "url": "/static/js/main.c1d923d6.chunk.js"
   },
   {
     "revision": "05a43ab45ddf252d239b",
-    "url": "/amre/static/js/runtime-main.28639572.js"
+    "url": "/static/js/runtime-main.28639572.js"
   }
 ]);
