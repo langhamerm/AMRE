3de875b7071c5e3ad7d931e5bc21b82f0a666863
Mark Langhamer <langhamerm@gmail.com>
2020-05-18 22:53:12 -0500

    Updates

---
+++ precache-manifest.9cd05aff2681df988878dd22c5a193c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "309f8d9a3b5c236d4866e2153d4659d3",
+    "url": "/amre/index.html"
+  },
+  {
+    "revision": "451aafdd4a21c9ec8303",
+    "url": "/amre/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "752b192f47a0b9301979",
+    "url": "/amre/static/css/main.7846461a.chunk.css"
+  },
+  {
+    "revision": "451aafdd4a21c9ec8303",
+    "url": "/amre/static/js/2.a6dffdb0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/amre/static/js/2.a6dffdb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "752b192f47a0b9301979",
+    "url": "/amre/static/js/main.7b52eda6.chunk.js"
+  },
+  {
+    "revision": "05a43ab45ddf252d239b",
+    "url": "/amre/static/js/runtime-main.28639572.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e931fc8965ef21be7a2a8543394c3998.js"
+  "/amre/precache-manifest.9cd05aff2681df988878dd22c5a193c6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/amre/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
